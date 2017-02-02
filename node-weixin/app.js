96d0b72008f7173d05388276206d293044d16404
dahai <1031487320@qq.com>
2017-02-02 14:35:56 +0800

    创建搜电影
    SDK接口的实现

--- node-weixin/app.js
+++ node-weixin/app.js
@@ -16,22 +16,120 @@ var tpl = heredoc(function () {
 		<html lang="en">
 		<head>
 			<meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1">
-			<title>猜电影</title>
+			<title>搜电影</title>
 		</head>
 		<body>
 			<h1>点击录音，开始翻译</h1>
 			<p id="title"></p>
+			<p id="directors"></p>
+			<p id="year"></p>
 			<div id="poster"></div>
 			<script src="http://zeptojs.com/zepto-docs.min.js"></script>
 			<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 			<script>
 				wx.config({
-				    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-				    appId: 'wx9226474d248fd396', // 必填，公众号的唯一标识
-				    timestamp: '<%=timestamp%>', // 必填，生成签名的时间戳
-				    nonceStr: '<%=noncestr%>', // 必填，生成签名的随机串
-				    signature: '<%=signature%>',// 必填，签名，见附录1
-				    jsApiList: ['startRecord','stopRecord','onVoiceRecordEnd','translateVoice'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+				    debug: false, 
+				    appId: 'wx9226474d248fd396', 
+				    timestamp: '<%=timestamp%>', 
+				    nonceStr: '<%=noncestr%>', 
+				    signature: '<%=signature%>',
+				    jsApiList: ['startRecord','stopRecord','onVoiceRecordEnd','translateVoice','onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone','previewImages']
+				});
+				wx.ready(function(){
+					wx.checkJsApi({
+					    jsApiList: ['onVoiceRecordEnd'],
+					    success: function(res) {
+					    	console.log(res)
+					    }
+					});
+
+
+					var shareContent = {
+					    title: '搜搜', 
+					    desc: '我搜出来了', 
+					    link: 'http://baidu.com/', 
+					    imgUrl: '', 
+					    type: 'link', 
+					    dataUrl: '', 
+					    success: function() { 
+					        window.alert('分享成功')
+					    },
+					    cancel: function() { 
+					        window.alert('取消分享')
+						}
+					}
+					var slides;
+					wx.onMenuShareAppMessage(shareContent);
+					
+					var isRecording = false;
+					$('#poster').on('tap', function() {
+						console.log(slides)
+						wx.previewImage(slides);
+					})
+					$('h1').on('tap', function() {
+						if(!isRecording){
+							isRecording = true
+							wx.startRecord({
+								cancel : function() {
+									window.alert('你点击取消，不搜索呢')	
+								}
+							});
+							return 
+						}
+						isRecording = false
+						wx.stopRecord({
+						    success: function (res) {
+						        var localId = res.localId;
+								wx.translateVoice({
+								   localId: localId, 
+								    isShowProgressTips: 1, 
+								    success: function (res) {
+								    	var reslut = res.translateResult;
+								    	$.ajax({
+									        url: 'http://api.douban.com/v2/movie/search?q=' + reslut,
+									        type: 'get',
+									        dataType: 'jsonp',
+									        jsonp: 'callback',
+									        success: function(data) {
+									        	var subjects = data.subjects[0]
+												$('#title').html(subjects.title);  
+												$('#directors').html(subjects.directors[0].name);  
+												$('#year').html(subjects.year); 
+												$('#poster').html('<img src="'+subjects.images.large+'"></img>');
+												shareContent = {				
+												    title: subjects.title, 
+												    desc: '我搜出来了' + subjects.title, 
+												    link: 'http://baidu.com/', 
+												    imgUrl: subjects.images.large,
+												    type: 'link', 
+    												dataUrl: '', 
+												    success: function() { 
+												        window.alert('分享成功,yeah!!!!')
+												    },
+												    cancel: function() { 
+												        window.alert('取消分享,no!!!!')
+					    							}
+												}
+
+												slides = {
+												    current: subjects.images.large, 
+												    urls: [subjects.images.large] 
+												}
+
+												data.subjects.forEach(function(item, index) {
+													if(index < 5){
+														slides.urls.push(item.images.large)
+													}
+												})
+												
+												wx.onMenuShareAppMessage(shareContent);  
+									        }
+									      }) 
+								    }
+								});
+						    }
+						});
+					})
 				});
 		</script>
 		</body>
@@ -77,7 +175,6 @@ app.use(function* (next) {
 		var ticket = ticketData.ticket;
 		var url = this.href;
 		var params = sign(ticket, url);
-		console.log(params);
 		this.body = ejs.render(tpl, params);
 		return next
 	}
